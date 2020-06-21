2d983f9c3e24c5a80fab91d9fa14c8cbdff7e123
ZeidmanRon <ronze@post.bgu.ac.il>
2020-06-21 17:36:36 +0300

    Calculator.py is almost ready

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,5 @@
-from tkinter import Tk, Label, Button, Entry, IntVar, END, W, E
+from tkinter import Tk, Label, Button, Entry, IntVar, END, W, E, filedialog
+from PreProcess import PreProcess
 
 
 class GUI:
@@ -6,6 +7,7 @@ class GUI:
     def __init__(self, master):
         self.master = master
         master.title("GUI")
+        master.geometry('400x200')
 
         self.total = 0
         self.entered_number = 0
@@ -16,23 +18,31 @@ class GUI:
 
         self.label = Label(master, text="Total:")
 
-        vcmd = master.register(self.validate) # we have to wrap the command
-        self.entry = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
-
         self.add_button = Button(master, text="+", command=lambda: self.update("add"))
         self.subtract_button = Button(master, text="-", command=lambda: self.update("subtract"))
         self.reset_button = Button(master, text="Reset", command=lambda: self.update("reset"))
 
+        #######################################################################################################
+
+        self.btn = Button(master, text="Browse file", command=lambda: self.browse(self.entry))
+        self.entry = Entry(master, width=50)
+        vcmd = master.register(self.validate)  # we have to wrap the command
+        self.cluster_label = Label(master, text="Number of clusters k")
+        self.entry2 = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
+        self.number_runs = Label(master, text="Number of runs")
+        self.entry3 = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
+        self.pre_process_btn = Button(master, text="Pre-process", command=lambda: PreProcess(self.entry.get()))
+        self.cluster = Button(master, text="Cluster", command=lambda: PreProcess(self.entry.get()))
+
         # LAYOUT
-
-        self.label.grid(row=0, column=0, sticky=W)
-        self.total_label.grid(row=0, column=1, columnspan=2, sticky=E)
-
-        self.entry.grid(row=1, column=0, columnspan=3, sticky=W+E)
-
-        self.add_button.grid(row=2, column=0)
-        self.subtract_button.grid(row=2, column=1)
-        self.reset_button.grid(row=2, column=2, sticky=W+E)
+        self.entry.grid(row=0, column=0, columnspan=6, sticky=W + E)
+        self.btn.grid(row=0, column=7, columnspan=2, sticky=W + E)
+        self.cluster_label.grid(row=1, column=0)
+        self.entry2.grid(row=1, column=2, sticky=W + E)
+        self.number_runs.grid(row=2, column=0)
+        self.entry3.grid(row=2, column=2, sticky=W + E)
+        self.pre_process_btn.grid(row=4, column=0)
+        self.cluster.grid(row=4, column=2)
 
     def validate(self, new_text):
         if not new_text:  # the field is being cleared
@@ -55,6 +65,13 @@ class GUI:
 
         self.total_label_text.set(self.total)
         self.entry.delete(0, END)
+
+    def browse(self, entry):
+        Tk().withdraw()
+        file_path = filedialog.askopenfilename()
+        entry.insert(0, file_path)
+        return
+
 
 root = Tk()
 my_gui = GUI(root)
